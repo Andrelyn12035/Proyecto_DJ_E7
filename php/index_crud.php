5624bb8debb595b4b96427c91911515ee862c9c9
José Antonio <morales.martinez.jose.antonio0@gmail.com>
2023-05-28 19:30:01 -0600

    Se agrega barra de navegación menu admin

--- php/index_crud.php
+++ php/index_crud.php
@@ -1,8 +1,4 @@
 <?php include("db.php"); ?>
 <?php include("header.php")?>
-<nav class="navbar navbar-dark bg-dark">
-        <div class="container">
-            <a href="index_crud.php" class="navbar-brand">AMINISTRADOR</a>
-        </div>
-</nav>
+
 <?php include("footer.php")?>
