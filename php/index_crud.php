0c017617bd13924c71a5764099efdc598c2a776d
José Antonio <morales.martinez.jose.antonio0@gmail.com>
2023-05-28 20:44:35 -0600

    Se agrego estructura de la tabla a menu de admin

--- php/index_crud.php
+++ php/index_crud.php
@@ -1,4 +1,27 @@
 <?php include("db.php"); ?>
 <?php include("header.php")?>
-
+    <div class="container-lg my-3 mx-4">
+    <a href="../form.html"><button type="button" class="btn btn-primary btn-lg btn-block ">AGREGAR</button></a>
+    
+    <h3 class="text-center  my-4">Cliente</h3>
+    
+    <table class="table table-bordered mt-4">
+        <tr>
+            <th>CURP</th>
+            <th>Nombre</th>
+            <th>AMaterno</th>
+            <th>APaterno</th>
+            <th>Num_cas</th>
+            <th>Colonia</th>
+            <th>Alcadia</th>
+            <th>CP</th>
+            <th>Estado</th>
+            <th>Email</th>
+            <th></th>
+        </tr>
+        <tbody>
+            <!--Consulatas a la base  -->
+        </tbody>
+    </table>
+    </div>
 <?php include("footer.php")?>
