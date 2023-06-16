d55b4b1daf1447fb295ce24b6c672435568374e9
Andrelyn12035 <kevo1803wow@gmail.com>
2023-06-15 21:39:19 -0600

    base de datos local

--- php/db.php
+++ php/db.php
@@ -1,19 +1,18 @@
 <?php
 
-/*
-   
    $conexion = mysqli_connect(
    'localhost',//Host
    'root',//user
    '',//password
    'bd_proyecto'//database
-   ) or die(mysqli_erro($mysqli));
-   */
+   ) or die(mysqli_error($mysqli));
+   /*
   $conexion = mysqli_connect(
    'proyecto-dj.cvnwmcsl2fhb.us-east-1.rds.amazonaws.com',//Host
    'admin',//user
    'chupabarritas01',//password
    'bd_proyecto'//database
-   ) or die(mysqli_erro($mysqli));
+   ) or die(mysqli_error($mysqli));
+   */
 ?>
 
