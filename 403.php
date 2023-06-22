eaa8ff153af1458e5fae4dcec86fcaa657d3d3de
Andrelyn12035 <kevo1803wow@gmail.com>
2023-06-21 18:50:14 -0600

    Errores php

---
+++ 403.php
@@ -0,0 +1,10 @@
+<?php
+  header("HTTP/1.0 403 Forbidden");
+  header("Location: /");
+?>
+<html>
+  <head>
+    <title>404 Error - Page Not Found</title>
+  </head>
+  <body>404 Errsdund!</body>
+</html>
---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php
+  header("HTTP/1.0 404 Not Found");
+  header("Location: /");
+?>
+<html>
+  <head>
+    <title>404 Error - Page Not Found</title>
+  </head>
+  <body>404 Errsdund!</body>
+</html>
